73fbe1503b66f3874140af91a88ed6d7f0c676a0
Ganesh Lokare <lokaregns@gmail.com>
2023-01-07 20:20:33 +0530

    folder structure created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+
+    name="X_Rays",
+    version="0.0.1",
+    author="Ganesh",
+    author_email="lokaregns@gmail.com",
+    packages= find_packages(),
+    install_requires=[],
+)
